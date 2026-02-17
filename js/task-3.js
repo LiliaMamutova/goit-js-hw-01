efc488500316d91724c18789ed09939ac3c365cc
Lilia Mamutova <lilymalinovskaya@gmail.com>
2026-02-18 00:38:55 +0200

    js_hw#1

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = Number.parseInt(content);
-    const paddingWidth = Number.parseInt(padding);
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding);
     const borderWidth = Number.parseFloat(border);
 
     return contentWidth + (paddingWidth * 2) + (borderWidth * 2);
